87bf54a76e8fc8edf3aa06187e0ebfca8212511d
Koushik Dey <koushik.dey@live.com>
2022-12-03 01:25:43 +0200

    :white_check_mark: ex2.11s
    done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <div align="center"><a href="https://fullstackopen.com/en/">Full Stack open 2022</a></div>
 
-<p align="center">To make my concept clearer and keep myself on the full stack track, I learn from this course and practice the exercises. This course is modern, updated, and full of useful information.</p>
+<p align="center">To make my concept clearer and keep myself on the full stack track, I practice the exercises. This course is updated, and full of useful information.</p>
 
--- src/App.js
+++ src/App.js
@@ -1,66 +1,11 @@
-// Sample test
-import { useState, useEffect } from "react";
-import Note from "./components/Note";
-import axios from 'axios';
+import React from "react";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState();
-  const [showAll, setShowAll] = useState(true);
-
-
-  useEffect(() => {
-    //console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        //console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  //console.log('render', notes.length, 'notes')
-
-  const addNote = (e) => {
-    e.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    };
-
-  setNotes(notes.concat(noteObject))
-  setNewNote('')
-  };
-
-  const handleNoteChange = (e) => {
-    console.log(e.target.value);
-    setNewNote(e.target.value);
-  };
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important);
-
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      
     </div>
-  );
-};
+  )
+}
 
 export default App;
