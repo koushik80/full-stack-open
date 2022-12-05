d0a09e27db1cabee8f67b74fb241ab5808da9c65
Koushik Dey <koushik.dey@live.com>
2022-12-05 02:14:13 +0200

    :bricks: part-2:phonebook:step7 and 8, ex-2.15 and 2.16
    done

---
+++ src/components/Persons.js
@@ -0,0 +1,17 @@
+import Person from "./Person";
+
+const Persons = ({ persons }) => {
+  return (
+      <div>
+          <ul>
+              {persons.map(person =>
+                <Person
+                  key={person.name}
+                  person={person} />
+              )}
+          </ul>
+      </div>
+  )
+}
+
+export default Persons;
